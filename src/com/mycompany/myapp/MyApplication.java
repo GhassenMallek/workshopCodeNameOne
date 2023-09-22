c5fb4f6f38b7f20c70ae3dcd3600061c067479f4
Ghassen Mallek <mallekghassen@gmail.com>
2023-09-22 13:47:55 +0100

    workshop1

--- src/com/mycompany/myapp/MyApplication.java
+++ src/com/mycompany/myapp/MyApplication.java
@@ -13,7 +13,6 @@ import com.codename1.ui.Toolbar;
 import java.io.IOException;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.io.NetworkEvent;
-import com.mycompany.myapp.GUI.ForgetPassword;
 import com.mycompany.myapp.GUI.SignIn;
 import com.mycompany.myapp.GUI.SignUp;
 
